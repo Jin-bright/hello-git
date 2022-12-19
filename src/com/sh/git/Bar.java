66df765e342896c9d98c80da53b80283ddaa80c5
Jin-bright <gnsdl4957@naver.com>
2022-12-19 11:20:18 +0900

    reset 이후 새로운 버전

--- src/com/sh/git/Bar.java
+++ src/com/sh/git/Bar.java
@@ -6,5 +6,8 @@ public class Bar {
 		
 	}
 	
+	public void bar2() {
+		
+	}
 
 }
--- src/com/sh/git/main.java
+++ src/com/sh/git/main.java
@@ -1,5 +1,15 @@
 package com.sh.git;
 
+/**
+ * 되돌리기 
+ * - revert : 지정한 커밋의 작업내용을 취소한 버전을 새로운 커밋으로 만든다.  (충돌날수있음 )   
+ * - reset : 지정한 커밋으로 돌아간다.
+ * 	 -hard : 지정커밋이후 변경사항 버림 
+ * 	 -mixed : 지정커밋이후변경사항을 unstaging area에 보관. 재추가 후 커밋 가능  
+ *   -soft : 지정 커밋이후 변경사항을 staging area에 보관 (즉시커밋가능) 
+ *
+ */
+
 public class main {
 
 	public static void main(String[] args) {
