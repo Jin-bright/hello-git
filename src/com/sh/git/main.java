652616e1c8782f2a19438e9074916f431e833b78
Jin-bright <gnsdl4957@naver.com>
2022-12-19 10:44:16 +0900

    01-hello-git

--- src/com/sh/git/main.java
+++ src/com/sh/git/main.java
@@ -3,7 +3,8 @@ package com.sh.git;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("Hello git");
 
 	}
 
